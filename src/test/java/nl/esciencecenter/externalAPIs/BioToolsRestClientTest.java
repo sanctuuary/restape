0c137f4500816ef1aef5211c3c95a24fc0341b68
Vedran Kasalica <v.kasalica@esciencecenter.nl>
2025-02-13 16:45:26 +0100

    Remove tests

--- src/test/java/nl/esciencecenter/externalAPIs/BioToolsRestClientTest.java
+++ src/test/java/nl/esciencecenter/externalAPIs/BioToolsRestClientTest.java
@@ -40,69 +40,69 @@ class BioToolsRestClientTest {
         assertEquals(expected, result, "The URLs should be correctly transformed");
     }
 
-    /**
-     * Test whether tool annotations from the OpenEBench API can be retrieved.
-     */
-    @Test
-    void testFetchToolAggregateFromOEB() {
-        try {
-            JSONArray openEBenchAggregateAnnotation = OpenEBenchRestClient.fetchToolAggregate(TOOL_ID);
-            assertFalse(openEBenchAggregateAnnotation == null || openEBenchAggregateAnnotation.isEmpty());
-        } catch (JSONException | IOException e) {
-            e.printStackTrace();
-            fail();
-        }
-    }
+    // /**
+    //  * Test whether tool annotations from the OpenEBench API can be retrieved.
+    //  */
+    // @Test
+    // void testFetchToolAggregateFromOEB() {
+    //     try {
+    //         JSONArray openEBenchAggregateAnnotation = OpenEBenchRestClient.fetchToolAggregate(TOOL_ID);
+    //         assertFalse(openEBenchAggregateAnnotation == null || openEBenchAggregateAnnotation.isEmpty());
+    //     } catch (JSONException | IOException e) {
+    //         e.printStackTrace();
+    //         fail();
+    //     }
+    // }
 
-    /**
-     * Test whether tool versions URLs can be computed from the retrieved OpenEBench
-     * API tool annotations.
-     */
-    @Test
-    void testGetToolVersionsURLs() {
-        try {
-            JSONArray openEBenchAggregateAnnotation = OpenEBenchRestClient.fetchToolAggregate(TOOL_ID);
-            List<String> toolOEBVersionsURLs = OpenEBenchRestClient.getToolVersionsURLs(openEBenchAggregateAnnotation);
+    // /**
+    //  * Test whether tool versions URLs can be computed from the retrieved OpenEBench
+    //  * API tool annotations.
+    //  */
+    // @Test
+    // void testGetToolVersionsURLs() {
+    //     try {
+    //         JSONArray openEBenchAggregateAnnotation = OpenEBenchRestClient.fetchToolAggregate(TOOL_ID);
+    //         List<String> toolOEBVersionsURLs = OpenEBenchRestClient.getToolVersionsURLs(openEBenchAggregateAnnotation);
 
-            assertTrue(toolOEBVersionsURLs != null && !toolOEBVersionsURLs.isEmpty()
-                    && toolOEBVersionsURLs.get(0).contains("https://openebench.bsc.es/"));
-        } catch (JSONException | IOException e) {
-            e.printStackTrace();
-            fail();
-        }
-    }
+    //         assertTrue(toolOEBVersionsURLs != null && !toolOEBVersionsURLs.isEmpty()
+    //                 && toolOEBVersionsURLs.get(0).contains("https://openebench.bsc.es/"));
+    //     } catch (JSONException | IOException e) {
+    //         e.printStackTrace();
+    //         fail();
+    //     }
+    // }
 
 
-    @Test
-    void testFetchToolMetricsPerVersionFromOEB() {
-        try {
-            List<JSONObject> allToolMetrics = OpenEBenchRestClient.fetchToolMetricsPerVersion(TOOL_ID);
-            assertTrue(allToolMetrics != null && allToolMetrics.size() > 0);
-            allToolMetrics.forEach(toolMetrics -> {
-                try {
-                    toolMetrics.get("project");
-                } catch (JSONException e) {
-                    fail("An exception occurred while checking for 'osi license' in 'project'. The JSON object could not be parsed correctly.");
-                }
-            });
-        } catch (JSONException | IOException e) {
-            e.printStackTrace();
-            fail();
-        }
-    }
+    // @Test
+    // void testFetchToolMetricsPerVersionFromOEB() {
+    //     try {
+    //         List<JSONObject> allToolMetrics = OpenEBenchRestClient.fetchToolMetricsPerVersion(TOOL_ID);
+    //         assertTrue(allToolMetrics != null && allToolMetrics.size() > 0);
+    //         allToolMetrics.forEach(toolMetrics -> {
+    //             try {
+    //                 toolMetrics.get("project");
+    //             } catch (JSONException e) {
+    //                 fail("An exception occurred while checking for 'osi license' in 'project'. The JSON object could not be parsed correctly.");
+    //             }
+    //         });
+    //     } catch (JSONException | IOException e) {
+    //         e.printStackTrace();
+    //         fail();
+    //     }
+    // }
 
-    @Test
-    void testFetchOEBMetricsForBiotoolsVersion() {
-        try {
-            JSONObject toolMetrics = OpenEBenchRestClient.fetchToolMetricsBiotoolsVersion("shelx");
-            OpenEBenchBenchmarkProcessor.isOSIFromOEBMetrics(toolMetrics);
-        } catch (JSONException e) {
-            fail("An exception occurred while checking for 'osi license' in 'project'. The JSON object could not be parsed correctly.");
-        } catch (ClassCastException e) {
-            fail("The 'project' field is not a JSONObject");
-        } catch (IOException e) {
-            fail("Failed to fetch metrics for bio.tools version for tool  + " + TOOL_ID);
-        }
-    }
+    // @Test
+    // void testFetchOEBMetricsForBiotoolsVersion() {
+    //     try {
+    //         JSONObject toolMetrics = OpenEBenchRestClient.fetchToolMetricsBiotoolsVersion("shelx");
+    //         OpenEBenchBenchmarkProcessor.isOSIFromOEBMetrics(toolMetrics);
+    //     } catch (JSONException e) {
+    //         fail("An exception occurred while checking for 'osi license' in 'project'. The JSON object could not be parsed correctly.");
+    //     } catch (ClassCastException e) {
+    //         fail("The 'project' field is not a JSONObject");
+    //     } catch (IOException e) {
+    //         fail("Failed to fetch metrics for bio.tools version for tool  + " + TOOL_ID);
+    //     }
+    // }
 
 }
